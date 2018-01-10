06e1fecb9b11433694b2c391dd7f6932bea17fde
Ben Kaminski <ben@benkaminski.com>
2018-01-10 13:20:10 -0500

    Aesthetic Changes
    Replaced modal with include file, created /inc directory for include files. Created "emergency-modal.php" for first include. Changed button styles for modal button.

--- footer.php
+++ footer.php
@@ -1,23 +1,4 @@
- 	<!-- Modal -->
-	<div class="modal fade" id="k911Emergency" tabindex="-1" role="dialog" aria-labelledby="k911EmergencyLabel" aria-hidden="true">
-	  <div class="modal-dialog" role="document">
-	    <div class="modal-content">
-	      <div class="modal-header">
-	        <h5 class="modal-title" id="k911EmergencyLabel"><i class="fa fa-ambulance" aria-hidden="true"></i> Is Your Pet in Danger?</h5>
-	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-	          <span aria-hidden="true">&times;</span>
-	        </button>
-	      </div>
-	      <div class="modal-body text-center">
-	       <button class="btn btn-danger btn-lg">Get Help Now!</button>
-	      </div>
-	      <div class="modal-footer">
-	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-	        <button type="button" class="btn btn-primary">Save changes</button>
-	      </div>
-	    </div>
-	  </div>
-	</div>
+ <?php include get_template_directory() . '/inc/emergency-modal.php'; ?>
  <?php wp_footer(); ?>
  </body>
 </html>
---
+++ inc/emergency-modal.php
@@ -0,0 +1,19 @@
+<!-- Modal -->
+	<div class="modal fade" id="k911Emergency" tabindex="-1" role="dialog" aria-labelledby="k911EmergencyLabel" aria-hidden="true">
+	  <div class="modal-dialog" role="document">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <h5 class="modal-title" id="k911EmergencyLabel"><i class="fa fa-ambulance" aria-hidden="true"></i> Is Your Pet in Danger?</h5>
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	      </div>
+	      <div class="modal-body text-center">
+	       <button class="btn btn-outline-danger btn-rounded btn-lg pl-5 pr-5">Get Help Now!</button>
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-secondary" data-dismiss="modal">No, Continue to Website</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
