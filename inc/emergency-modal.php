74a92ca9aca02ae122d92c2714c99c2fbb536e54
Ben Kaminski <ben@benkaminski.com>
2018-01-10 15:15:16 -0500

    Added theme colors
    Added theme colors to modal header and footer. Created CSS for styles.

--- inc/emergency-modal.php
+++ inc/emergency-modal.php
@@ -1,14 +1,14 @@
 <!-- Modal -->
 	<div class="modal fade bd-example-modal-lg" id="k911Emergency" tabindex="-1" role="dialog" aria-labelledby="k911EmergencyLabel" aria-hidden="true">
-	  <div class="modal-dialog" role="document">
+	  <div class="modal-dialog modal-dialog-centered" role="document">
 	    <div class="modal-content">
-	      <div class="modal-header">
-	        <h5 class="modal-title" id="k911EmergencyLabel"><i class="fa fa-ambulance" aria-hidden="true"></i> Is Your Pet in Danger?</h5>
+	      <div class="modal-header emergency-modal-header">
+	        <h3 class="modal-title emergency-modal-title text-white" id="k911EmergencyLabel"><i class="fa fa-ambulance fa-lg" aria-hidden="true"></i> Is Your Pet in Danger?</h3>
 	      </div>
 	      <div class="modal-body text-center">
-	       <button class="btn btn-outline-danger btn-rounded btn-lg pl-5 pr-5 mt-2 text-uppercase">Get Help Now</button>
+	       <button class="btn btn-outline-danger btn-rounded btn-lg pl-5 pr-5 mt-5 mb-5 text-uppercase"><i class="fa fa-mobile fa-lg mr-2" aria-hidden="true"></i><strong>Get Help Now</strong></button>
 	      </div>
-	      <div class="modal-footer mt-2">
+	      <div class="modal-footer emergency-modal-footer mt-2">
 	        <button type="button" class="btn btn-warning" data-dismiss="modal">No, Continue to Website</button>
 	      </div>
 	    </div>
