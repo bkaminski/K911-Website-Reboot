75f6cf6b14784a02cba5ca0ad5295a21edc6c5cd
Luci Karnai <luci.karnai@gmail.com>
2018-01-10 14:14:15 -0500

    Styled btn-warning
    Changed button warning colors to match theme colors. Added CSS to globally override btn-warning.

--- inc/emergency-modal.php
+++ inc/emergency-modal.php
@@ -1,18 +1,15 @@
 <!-- Modal -->
-	<div class="modal fade" id="k911Emergency" tabindex="-1" role="dialog" aria-labelledby="k911EmergencyLabel" aria-hidden="true">
+	<div class="modal fade bd-example-modal-lg" id="k911Emergency" tabindex="-1" role="dialog" aria-labelledby="k911EmergencyLabel" aria-hidden="true">
 	  <div class="modal-dialog" role="document">
 	    <div class="modal-content">
 	      <div class="modal-header">
 	        <h5 class="modal-title" id="k911EmergencyLabel"><i class="fa fa-ambulance" aria-hidden="true"></i> Is Your Pet in Danger?</h5>
-	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-	          <span aria-hidden="true">&times;</span>
-	        </button>
 	      </div>
 	      <div class="modal-body text-center">
-	       <button class="btn btn-outline-danger btn-rounded btn-lg pl-5 pr-5">Get Help Now!</button>
+	       <button class="btn btn-outline-danger btn-rounded btn-lg pl-5 pr-5 mt-2 text-uppercase">Get Help Now</button>
 	      </div>
-	      <div class="modal-footer">
-	        <button type="button" class="btn btn-secondary" data-dismiss="modal">No, Continue to Website</button>
+	      <div class="modal-footer mt-2">
+	        <button type="button" class="btn btn-warning" data-dismiss="modal">No, Continue to Website</button>
 	      </div>
 	    </div>
 	  </div>
