e6b434c325a8ee566062c6a64faa581c22f77796
Ben Kaminski <ben@benkaminski.com>
2018-01-10 19:50:10 -0500

    Some style swaps CSS
    I changed the modal header color to yellow and the button to blue. Essentially reversing the order of things in the modal. I thought the yellow in the header was more "emergent".

--- inc/emergency-modal.php
+++ inc/emergency-modal.php
@@ -3,13 +3,13 @@
 	  <div class="modal-dialog modal-dialog-centered" role="document">
 	    <div class="modal-content">
 	      <div class="modal-header emergency-modal-header">
-	        <h3 class="modal-title emergency-modal-title text-white" id="k911EmergencyLabel"><i class="fa fa-ambulance fa-lg" aria-hidden="true"></i> Is Your Pet in Danger?</h3>
+	        <h3 class="modal-title emergency-modal-title" id="k911EmergencyLabel"><i class="fa fa-ambulance fa-lg" aria-hidden="true"></i> Is Your Pet in Danger?</h3>
 	      </div>
 	      <div class="modal-body text-center">
 	       <button class="btn btn-outline-danger btn-rounded btn-lg pl-5 pr-5 mt-5 mb-5 text-uppercase"><i class="fa fa-mobile fa-lg mr-2" aria-hidden="true"></i><strong>Get Help Now</strong></button>
 	      </div>
 	      <div class="modal-footer emergency-modal-footer mt-2">
-	        <button type="button" class="btn btn-warning" data-dismiss="modal">No, Continue to Website</button>
+	        <button type="button" class="btn btn-info" data-dismiss="modal">No, Continue to Website</button>
 	      </div>
 	    </div>
 	  </div>
