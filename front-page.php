4cd7a64cfbba93333ace2d5e34157d8b13c4f855
Ben Kaminski <ben@benkaminski.com>
2018-01-20 11:30:11 -0500

    Began main content under header
    Began integration of Bootstrap Carousel and information area. Using Bootstrap grid and some royalty free images I found. Style updates, created an include file for the carousel for easier editing while in DEV. Eventually to be replaced with WP Theme customizer functionality.

--- front-page.php
+++ front-page.php
@@ -1,3 +1,14 @@
 <?php get_header(); ?>
+	<section class="k911-home">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-4 k911-info">
 
+				</div>
+				<div class="col-md-8 p-0 k911-carousel">
+					<?php include get_template_directory() . '/inc/carousel.php'; ?>
+				</div>
+			</div>
+		</div>
+	</section>
 <?php get_footer(); ?>
---
+++ inc/carousel.php
@@ -0,0 +1,40 @@
+<div id="homePageCarousel" class="carousel slide" data-ride="carousel">
+	<div class="carousel-inner">
+    	<div class="carousel-item active">
+      		<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/media/dog1.jpg" alt="K911 Pet Transport Service">
+      		<div class="carousel-caption d-none d-md-block">
+    			<h5>...</h5>
+    			<p>...</p>
+  			</div>
+    	</div>
+    	<div class="carousel-item">
+      		<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/media/cat1.jpg" alt="Second slide">
+      		<div class="carousel-caption d-none d-md-block">
+    			<h5>...</h5>
+    			<p>...</p>
+  			</div>
+    	</div>
+    	<div class="carousel-item">
+      		<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/media/dog2.jpg" alt="Third slide">
+      		<div class="carousel-caption d-none d-md-block">
+    			<h5>...</h5>
+    			<p>...</p>
+  			</div>
+    	</div>
+    	<div class="carousel-item">
+      		<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/media/cat2.jpg" alt="Third slide">
+      		<div class="carousel-caption d-none d-md-block">
+    			<h5>...</h5>
+    			<p>...</p>
+  			</div>
+    	</div>
+  	</div>
+  	<a class="carousel-control-prev" href="#homePageCarousel" role="button" data-slide="prev">
+    	<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    	<span class="sr-only">Previous</span>
+  	</a>
+  	<a class="carousel-control-next" href="#homePageCarousel" role="button" data-slide="next">
+    	<span class="carousel-control-next-icon" aria-hidden="true"></span>
+    	<span class="sr-only">Next</span>
+  	</a>
+</div>
