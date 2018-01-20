18b6a5831b46687c10271b056868b4adc7ba521f
Ben Kaminski <ben@benkaminski.com>
2018-01-20 12:09:47 -0500

    Added compressed carousel images.
    Working with image resize in carousel. Not quite done. Compressed and resized images. Need Bootstrap Carousel to have a fixed height.

--- inc/carousel.php
+++ inc/carousel.php
@@ -1,31 +1,31 @@
 <div id="homePageCarousel" class="carousel slide" data-ride="carousel">
-	<div class="carousel-inner">
+	<div class="carousel-inner" style="height: 400px;">
     	<div class="carousel-item active">
       		<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/media/dog1.jpg" alt="K911 Pet Transport Service">
       		<div class="carousel-caption d-none d-md-block">
-    			<h5>...</h5>
-    			<p>...</p>
+    			<h5>Say Something Here</h5>
+    			<p>You can add some brief descriptions as well.</p>
   			</div>
     	</div>
     	<div class="carousel-item">
       		<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/media/cat1.jpg" alt="Second slide">
       		<div class="carousel-caption d-none d-md-block">
-    			<h5>...</h5>
-    			<p>...</p>
+    			<h5>Things Are Getting Done</h5>
+    			<p>As you can see, things are coming along.</p>
   			</div>
     	</div>
     	<div class="carousel-item">
       		<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/media/dog2.jpg" alt="Third slide">
       		<div class="carousel-caption d-none d-md-block">
-    			<h5>...</h5>
-    			<p>...</p>
+    			<h5>New Website</h5>
+    			<p>This is part of our new website design.</p>
   			</div>
     	</div>
     	<div class="carousel-item">
       		<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/media/cat2.jpg" alt="Third slide">
       		<div class="carousel-caption d-none d-md-block">
-    			<h5>...</h5>
-    			<p>...</p>
+    			<h5>Helping Animals</h5>
+    			<p>That's what we do round these parts.</p>
   			</div>
     	</div>
   	</div>
