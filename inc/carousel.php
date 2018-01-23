1b65298a1b8b8366cbf59bdb48674b59c9dbc23d
Ben Kaminski <ben@benkaminski.com>
2018-01-22 22:50:28 -0500

    Got Carousel working.
    Had to crop images to 798px height to avoid movement. Added all new compressed png's. Added styles where necessary to override Bootstrap Carousel functions.

--- inc/carousel.php
+++ inc/carousel.php
@@ -1,40 +1,69 @@
-<div id="homePageCarousel" class="carousel slide" data-ride="carousel">
-	<div class="carousel-inner" style="height: 400px;">
+<div id="k911Carousel" class="carousel slide" data-ride="carousel" data-interval="10000">
+	<ol class="carousel-indicators">
+    	<li data-target="#k911Carousel" data-slide-to="0" class="active"></li>
+        <li data-target="#k911Carousel" data-slide-to="1"></li>
+        <li data-target="#k911Carousel" data-slide-to="2"></li>
+        <li data-target="#k911Carousel" data-slide-to="3"></li>
+        <li data-target="#k911Carousel" data-slide-to="4"></li>
+    </ol>
+    <div class="carousel-inner">
     	<div class="carousel-item active">
-      		<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/media/dog1.jpg" alt="K911 Pet Transport Service">
-      		<div class="carousel-caption d-none d-md-block">
-    			<h5>Say Something Here</h5>
-    			<p>You can add some brief descriptions as well.</p>
-  			</div>
-    	</div>
-    	<div class="carousel-item">
-      		<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/media/cat1.jpg" alt="Second slide">
-      		<div class="carousel-caption d-none d-md-block">
-    			<h5>Things Are Getting Done</h5>
-    			<p>As you can see, things are coming along.</p>
-  			</div>
-    	</div>
-    	<div class="carousel-item">
-      		<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/media/dog2.jpg" alt="Third slide">
-      		<div class="carousel-caption d-none d-md-block">
-    			<h5>New Website</h5>
-    			<p>This is part of our new website design.</p>
-  			</div>
-    	</div>
-    	<div class="carousel-item">
-      		<img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/media/cat2.jpg" alt="Third slide">
-      		<div class="carousel-caption d-none d-md-block">
-    			<h5>Helping Animals</h5>
-    			<p>That's what we do round these parts.</p>
-  			</div>
-    	</div>
-  	</div>
-  	<a class="carousel-control-prev" href="#homePageCarousel" role="button" data-slide="prev">
+        	<img class="first-slide img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/media/cat01.png" alt="First slide">
+            <div class="container">
+            	<div class="carousel-caption text-left">
+                	<h1>Example headline.</h1>
+                	<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                	<p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+              	</div>
+            </div>
+        </div>
+        <div class="carousel-item">
+        	<img class="second-slide img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/media/dog01.png" alt="Second slide">
+            <div class="container">
+            	<div class="carousel-caption">
+                	<h1>Another example headline.</h1>
+                	<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                	<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+              	</div>
+            </div>
+        </div>
+        <div class="carousel-item">
+        	<img class="third-slide img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/media/cat03.png" alt="Third slide">
+            <div class="container">
+            	<div class="carousel-caption text-right">
+                	<h1>One more for good measure.</h1>
+                	<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                	<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+              	</div>
+            </div>
+        </div>
+        <div class="carousel-item">
+        	<img class="fourth-slide img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/media/bird01.png" alt="Fourth slide">
+            <div class="container">
+            	<div class="carousel-caption text-right">
+                	<h1>One more for good measure.</h1>
+                	<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                	<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+              	</div>
+            </div>
+        </div>
+        <div class="carousel-item">
+        	<img class="fifth-slide img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/media/dog02.png" alt="Fifth Slide">
+            <div class="container">
+            	<div class="carousel-caption text-right">
+                	<h1>One more for good measure.</h1>
+                	<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                	<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+              	</div>
+            </div>
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#k911Carousel" role="button" data-slide="prev">
     	<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    	<span class="sr-only">Previous</span>
-  	</a>
-  	<a class="carousel-control-next" href="#homePageCarousel" role="button" data-slide="next">
-    	<span class="carousel-control-next-icon" aria-hidden="true"></span>
-    	<span class="sr-only">Next</span>
-  	</a>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#k911Carousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
 </div>
