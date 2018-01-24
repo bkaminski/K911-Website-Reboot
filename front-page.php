5918bdca55afad99e73e4698b82e1a134be46538
Ben Kaminski <ben@benkaminski.com>
2018-01-23 20:44:51 -0500

    Fixed carousel images, added content to info area.
    Added new, compressed, images for image slide show AKA "carousel". Ensured images were same size height and width to ensure no sloppiness. Added CSS styles for info-area, background texture and background image added. Added a box-shadow to the left side of the carousel. Created media queries to handle top navigation, still needs work.

--- front-page.php
+++ front-page.php
@@ -2,8 +2,9 @@
 	<section class="k911-home">
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-md-4 k911-info">
-
+				<div class="col-md-4 k911-info pt-2">
+					<h1 class="text-center"><strong>K911 Animal Transport</strong></h1>
+					<p class="text-center">This is a test.</p>
 				</div>
 				<div class="col-md-8 p-0 k911-carousel">
 					<?php include get_template_directory() . '/inc/carousel.php'; ?>
--- inc/carousel.php
+++ inc/carousel.php
@@ -8,62 +8,57 @@
     </ol>
     <div class="carousel-inner">
     	<div class="carousel-item active">
-        	<img class="first-slide img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/media/cat01.png" alt="First slide">
+        	<img class="first-slide img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/media/cat001.png" alt="First slide">
             <div class="container">
             	<div class="carousel-caption text-left">
-                	<h1>Example headline.</h1>
-                	<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                	<p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+                	<h1 class="text-shadow">Example headline One.</h1>
+                	<p class="text-shadow">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
               	</div>
             </div>
         </div>
         <div class="carousel-item">
-        	<img class="second-slide img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/media/dog01.png" alt="Second slide">
+        	<img class="second-slide img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/media/dog001.png" alt="Second slide">
             <div class="container">
-            	<div class="carousel-caption">
-                	<h1>Another example headline.</h1>
-                	<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                	<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+            	<div class="carousel-caption text-left">
+                	<h1 class="text-shadow">Example Headline Two.</h1>
+                	<p class="text-shadow">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
               	</div>
             </div>
         </div>
         <div class="carousel-item">
-        	<img class="third-slide img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/media/cat03.png" alt="Third slide">
+        	<img class="third-slide img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/media/cat002.png" alt="Third slide">
             <div class="container">
-            	<div class="carousel-caption text-right">
-                	<h1>One more for good measure.</h1>
-                	<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                	<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+            	<div class="carousel-caption text-left">
+                	<h1 class="text-shadow">Example Headline Three.</h1>
+                	<p class="text-shadow">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
               	</div>
             </div>
         </div>
         <div class="carousel-item">
-        	<img class="fourth-slide img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/media/bird01.png" alt="Fourth slide">
+        	<img class="fourth-slide img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/media/bird001.png" alt="Fourth slide">
             <div class="container">
-            	<div class="carousel-caption text-right">
-                	<h1>One more for good measure.</h1>
-                	<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                	<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+            	<div class="carousel-caption text-left">
+                	<h1 class="text-shadow">Example Headline Four.</h1>
+                	<p class="text-shadow">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
               	</div>
             </div>
         </div>
         <div class="carousel-item">
-        	<img class="fifth-slide img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/media/dog02.png" alt="Fifth Slide">
+        	<img class="fifth-slide img-fluid d-block" src="<?php echo get_template_directory_uri(); ?>/media/dog002.png" alt="Fifth Slide">
             <div class="container">
-            	<div class="carousel-caption text-right">
-                	<h1>One more for good measure.</h1>
-                	<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                	<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+            	<div class="carousel-caption text-left">
+                	<h1 class="text-shadow">Example Headline Five.</h1>
+                	<p class="text-shadow">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
               	</div>
             </div>
         </div>
     </div>
     <a class="carousel-control-prev" href="#k911Carousel" role="button" data-slide="prev">
     	<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
+        <span class="sr-only">Previous Image</span>
     </a>
-    <a class="carousel-control-next" href="#k911Carousel" role="button" data-slide="next">
+    <a class="carousel-control-next text-shadow" href="#k911Carousel" role="button" data-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
+        <span class="sr-only">Next Image</span>
     </a>
 </div>
