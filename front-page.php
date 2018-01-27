1deb654777cfb68a23064d9d291f0c4ef90c2494
Ben Kaminski <ben@benkaminski.com>
2018-01-27 18:18:25 -0500

    Added logo image into page, removed as background CSS, works better. Added some copy from the current website, styles removed and added to CSS

--- front-page.php
+++ front-page.php
@@ -4,7 +4,8 @@
 			<div class="row">
 				<div class="col-md-4 k911-info pt-2">
 					<h1 class="text-center"><strong>K911 Animal Transport</strong></h1>
-					<p class="text-center">This is a test.</p>
+					<p class="text-center">We are <strong>K911</strong>, your compassionate, reliable, 24 hour <u>emergency</u> and <u>non-emergency</u> animal transport company.</p>
+					<img class="img-fluid mx-auto d-block" height="300" width="300" src="<?php echo get_template_directory_uri(); ?>/media/k911-web-logo.png" alt="K911 Logo" />
 				</div>
 				<div class="col-md-8 p-0 k911-carousel">
 					<?php include get_template_directory() . '/inc/carousel.php'; ?>
