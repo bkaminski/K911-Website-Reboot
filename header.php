894bf0f61b14106359b468765eed3b2f612b7e35
Ben Kaminski <ben@benkaminski.com>
2018-02-01 19:43:59 -0500

    Added pulsating "get help" button
    Also added an include file for the responsive tables needed for the transport fees document.

--- header.php
+++ header.php
@@ -46,7 +46,7 @@
 	        );
         ?>
         <form class="form-inline my-2 my-lg-0">
-          <button class="btn btn-danger btn-lg my-2 my-sm-0 text-uppercase btn-rounded" type="submit">Get Help Now</button>
+          <button class="btn btn-danger btn-lg my-2 my-sm-0 d-none d-lg-block text-uppercase btn-rounded mr-3 help-button" type="submit">Get Help Now</button>
         </form>
       </div>
     </nav>
