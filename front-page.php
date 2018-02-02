804fd6d0b44ffa00e969d57c8f6c3f3cb045047d
Ben Kaminski <ben@benkaminski.com>
2018-02-01 19:03:09 -0500

    Added responsive fonts
    To carousel and title areas. Also created new CSS classes.

--- front-page.php
+++ front-page.php
@@ -34,17 +34,17 @@
   			</div>
   		</div>
 	</section>
-	<section class="k911-content">
+	<main class="k911-content">
 		<div class="container pt-5 pb-5">
 			<div class="row">
 				<div class="col-md-12">
 					<p class="text-center"><i class="fas fa-ambulance fa-5x" style="color: #4b88a2;"></i></p>
-					<h2 class="display-4 text-center mb-5">We specialize in pet transportation to and from veterinary practices, airports, and more throughout the Delaware Valley.</h2>
+					<h2 class="display-4 k911-mission text-center mb-5">We specialize in pet transportation to and from veterinary practices, airports, and more throughout the Delaware Valley.</h2>
 					<h3 class="display-5 text-center text-info">Our staff of experienced animal handlers are available 24 hours a day, 7 days a week.</h3>
 				</div>
 			</div>
 		</div>
-	</section>
+	</main>
 	<section class="k911-content-2">
 		<div class="container pb-5 pt-5">
 			<div class="row">
--- inc/carousel.php
+++ inc/carousel.php
@@ -11,8 +11,8 @@
         	<img class="first-slide img-fluid d-block k911CarouselImg" src="<?php echo get_template_directory_uri(); ?>/media/cat001.png" alt="First slide">
             <div class="container">
             	<div class="carousel-caption text-left">
-                	<h1 class="text-shadow">Example headline One.</h1>
-                	<p class="text-shadow">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                	<h1 class="text-shadow k911-carousel-title">Example headline One.</h1>
+                	<p class="text-shadow d-none d-sm-block">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
               	</div>
             </div>
         </div>
@@ -20,8 +20,8 @@
         	<img class="second-slide img-fluid d-block k911CarouselImg" src="<?php echo get_template_directory_uri(); ?>/media/dog001.png" alt="Second slide">
             <div class="container">
             	<div class="carousel-caption text-left">
-                	<h1 class="text-shadow">Example Headline Two.</h1>
-                	<p class="text-shadow">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                	<h1 class="text-shadow k911-carousel-title">Example Headline Two.</h1>
+                	<p class="text-shadow d-none d-sm-block">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
               	</div>
             </div>
         </div>
@@ -29,8 +29,8 @@
         	<img class="third-slide img-fluid d-block k911CarouselImg" src="<?php echo get_template_directory_uri(); ?>/media/cat002.png" alt="Third slide">
             <div class="container">
             	<div class="carousel-caption text-left">
-                	<h1 class="text-shadow">Example Headline Three.</h1>
-                	<p class="text-shadow">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                	<h1 class="text-shadow k911-carousel-title">Example Headline Three.</h1>
+                	<p class="text-shadow d-none d-sm-block">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
               	</div>
             </div>
         </div>
@@ -38,8 +38,8 @@
         	<img class="fourth-slide img-fluid d-block k911CarouselImg" src="<?php echo get_template_directory_uri(); ?>/media/bird001.png" alt="Fourth slide">
             <div class="container">
             	<div class="carousel-caption text-left">
-                	<h1 class="text-shadow">Example Headline Four.</h1>
-                	<p class="text-shadow">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                	<h1 class="text-shadow k911-carousel-title">Example Headline Four.</h1>
+                	<p class="text-shadow d-none d-sm-block">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
               	</div>
             </div>
         </div>
@@ -47,8 +47,8 @@
         	<img class="fifth-slide img-fluid d-block k911CarouselImg" src="<?php echo get_template_directory_uri(); ?>/media/dog002.png" alt="Fifth Slide">
             <div class="container">
             	<div class="carousel-caption text-left">
-                	<h1 class="text-shadow">Example Headline Five.</h1>
-                	<p class="text-shadow">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                	<h1 class="text-shadow k911-carousel-title">Example Headline Five.</h1>
+                	<p class="text-shadow d-none d-sm-block">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
               	</div>
             </div>
         </div>
