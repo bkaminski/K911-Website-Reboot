9ae573b1af2c0d28d5fb578ec0e56bac808e112b
Ben Kaminski <ben@benkaminski.com>
2018-02-22 16:20:27 -0500

    moar moar cleanup

--- inc/carousel.php
+++ inc/carousel.php
@@ -11,10 +11,10 @@ $the_query = new WP_Query ( $args );
 <div id="k911Carousel" class="carousel slide" data-ride="carousel" data-interval="7000">
 	<ol class="carousel-indicators">
 
-		
+
 		<!-- Start first Loop -->
 		<?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-		<li data-target="#k911Carousel" data-slide-to="<?php echo $the_query->current_post; ?>" class="<?php if ( $the_query->current_post == 0 ) : ?>active<?php endif; ?>"></li>
+			<li data-target="#k911Carousel" data-slide-to="<?php echo $the_query->current_post; ?>" class="<?php if ( $the_query->current_post == 0 ) : ?>active<?php endif; ?>"></li>
 
 
 		<!-- end first loop -->
@@ -28,60 +28,63 @@ $the_query = new WP_Query ( $args );
 	<?php rewind_posts(); ?>
 
 
-    <div class="carousel-inner"> 	
-		
+	<div class="carousel-inner">
 
 
 		<!-- start second loop with args -->
 		<?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post();
-	          $thumbnail_id   = get_post_thumbnail_id();
-	          $thumbnail_url  = wp_get_attachment_image_src( $thumbnail_id, 'full', true );
-	          $thumbnail_meta = get_post_meta( $thumbnail_id, '_wp_attatchment_image_alt', true );
-	    ?>
+			$thumbnail_id   = get_post_thumbnail_id();
+			$thumbnail_url  = wp_get_attachment_image_src( $thumbnail_id, 'full', true );
+			$thumbnail_meta = get_post_meta( $thumbnail_id, '_wp_attatchment_image_alt', true );
+		?>
 
-	    
 
-	    <!-- set class to "active" if current post -->
-    	<div class="carousel-item <?php if ( $the_query->current_post == 0 ) : ?>active<?php endif; ?>">
-    		
+		<!-- set class to "active" if current post -->
+		<div class="carousel-item <?php if ( $the_query->current_post == 0 ) : ?>active<?php endif; ?>">
 
-    		<!-- if post has feattured-image, add to Bootstrap carousel, link image to article page -->
-    		<?php if ( has_post_thumbnail() ) : ?>
-    			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
 
-    				<!-- featured image from post -->
-    				<?php the_post_thumbnail('full'); ?>
+			<!-- if post has feattured-image, add to Bootstrap carousel, link image to article page -->
+			<?php if ( has_post_thumbnail() ) : ?>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+
+
+					<!-- featured image from post -->
+					<?php the_post_thumbnail('full'); ?>
+						
 
 
 				</a>
 			<?php endif; ?>
 
 
-	    	<div class="container">
-	    		<div class="carousel-caption text-left">
-	    			<h1 class="text-shadow k911-carousel-title">
+			<div class="container">
+				<div class="carousel-caption text-left">
+					<h1 class="text-shadow k911-carousel-title">
 
 
-	    				<!-- Article title -->
-	    				<?php the_title(); ?>
+						<!-- Article title -->
+						<?php the_title(); ?>
+							
 
 
-	    			</h1>
-	    			<span class="text-shadow d-none d-sm-block pb-2">
+					</h1>
+					<span class="text-shadow d-none d-sm-block pb-2">
 
-	    				<!-- excerpt of article without "read more" button -->
-	    				<?php $nobutton_excerpt = get_the_excerpt();
+
+						<!-- excerpt of article without "read more" button -->
+						<?php $nobutton_excerpt = get_the_excerpt();
 							if ( '' != $nobutton_excerpt ) {}
 							echo $nobutton_excerpt;
 						?>
+					
 
-	    			</span>
-	    		</div>
-	    	</div>
-    	</div>
+					</span>
+				</div>
+			</div>
+		</div>
 
 
-    	<!-- end second loop -->
-    	<?php endwhile;	endif; ?>
+		<!-- end second loop -->
+		<?php endwhile;	endif; ?>
     </div>
 </div>
