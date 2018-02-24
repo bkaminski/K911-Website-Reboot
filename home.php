842dc9540629eb8721e6264eb4621c7e8dfc72f9
Ben Kaminski <ben@benkaminski.com>
2018-02-23 21:12:01 -0500

    removed comments from home.php

--- home.php
+++ home.php
@@ -8,57 +8,26 @@
 						</div>
 						<div class="mt-4 k911-main">
 							<div class="row">
-
-
-								<!-- start WordPress Loop -->
 								<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 								<div class="col-md-3 pt-3">
-
-
-
-									<!-- call in featured image with link to page -->
 									<?php if ( has_post_thumbnail() ) : ?>
 	    								<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
 	        								<?php the_post_thumbnail('medium'); ?>
 	    								</a>
 									<?php endif; ?>
-
-
-
 								</div>
 								<div class="col-md-9">
-
-
-										<!-- call in article title -->
 										<h1><a href='<?php the_permalink() ?>'rel='bookmark' title='<?php the_title(); ?>'><?php the_title(); ?></a></h1>
 										<span class="badge badge-success">Date Posted: <?php the_time('m-d-Y') ?></span>
-
-
-										<!-- all content typed in WordPress Page editor -->
 										<?php the_excerpt(); ?>
 										<hr>
-										
-
 								</div>	
-
-
-
-								<!-- end WordPress Loop -->
 								<?php endwhile; else : ?>
 									<p><?php esc_html_e( 'We apologize... something went wrong. Please try again.' ); ?></p>
 								<?php endif; ?>
-
-
-
-
 							</div>
 						</div>
-
-
-						<!-- call in pagination -->
 						<?php echo k911_pagination(); ?>
-
-
 				</div>
 			</div>
 	</main>
