1c427159eab5e2fc332cbdae07a6849d00e64138
Ben Kaminski <ben@benkaminski.com>
2018-02-27 12:23:26 -0500

    added jQuery for lost password page

--- js/admin-page.js
+++ js/admin-page.js
@@ -4,12 +4,14 @@ $(document).ready(function () {
     $("#loginform").find('p').addClass('text-center');
     $("#registerform").addClass('form-group admin-login').css("border", "2px solid #000");
     $("#registerform").find('p').addClass('text-center');
+    $("#lostpasswordform").addClass('form-group admin-login').css("border", "2px solid #000").attr('size',20);
+    $("#lostpasswordform").find('p').addClass('text-center');
     $("#user_login").addClass('form-control').css("box-shadow", "inset 0 0 5px #000");
     $("#user_pass").addClass('form-control').css("box-shadow", "inset 0 0 5px #000");
     $("#user_email").addClass('form-control').css("box-shadow", "inset 0 0 5px #000").attr('size',20);
     $("#wp-submit").removeClass('button button-primary button-large').addClass('btn btn-info btn-rounded btn-lg pr-5 pl-5');
     $(".login form .forgetmenot").addClass('text-center mb-3');
     $(".message").removeClass('message').addClass('alert alert-info text-center');
-    $("#nav").addClass('text-center').find('a').addClass('btn btn-success btn-rounded text-white');
+    $("#nav").addClass('text-center').find('a').addClass('btn btn-success btn-sm btn-rounded text-white pr-5 pl-5');
     $("#backtoblog").addClass('text-center');
 });
