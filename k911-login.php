80d5c6cbb533ecc68f87d63d061846e27e98260d
Ben Kaminski <ben@benkaminski.com>
2018-03-01 08:38:00 -0500

    code cleanup

--- k911-login.php
+++ k911-login.php
@@ -13,77 +13,53 @@ get_header(); ?>
 				<div class="col-lg-12">
 					<!-- start WordPress Loop -->
 					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-						
-
 						<div class="alert alert-info mt-3">
 							<h1 class="display-4 text-shadow">
-								
 								<!-- title entered in WordPress page editor -->
 								<?php the_title(); ?>
-
-
 							</h1>
 						</div>
 							<div class="mt-4 k911-main">
-
 								<div class="col-md-6 mx-auto d-block">
 									<div class="card text-white bg-secondary mb-3">
   										<div class="card-header text-center">K911 User Login:</div>
-								  	<div class="card-body">
-								    	<!-- call user login form -->
-										<?php if ( ! is_user_logged_in() ) { 
-										    $args = array(
-										        'redirect' => admin_url(), 
-										        'form_id' => 'loginform-custom',
-										        'label_username' => __( 'Username:' ),
-										        'label_password' => __( 'Password:' ),
-										        'label_remember' => __( 'Remember Me' ),
-										        'label_log_in' => __( 'Log In' ),
-										        'remember' => true
-										    );
-										wp_login_form( $args ); ?>
-										<!-- end login form -->
-										<?php
-											} else {
-												echo "<div class='alert alert-success text-center'>You're already logged in</div>";
-										?>
-								  	</div>
-								  		<?php
-												echo "<div class='card-footer'>";
-											    echo "<div class='row'>";
-												
-
-
-													echo "<div class='col-sm-6'>
-															<span class='k911-loginout'>";	
-												    			wp_loginout( home_url() );
-												    echo "</span></div>"; 
-											    
-											    	echo "<div class='col-sm-6'>
-											    			<span class='k911-adminregister'>";
-											    				wp_register('', ''); 
-											    	echo "</span></div>";
-
-											    echo "</div>";
-											    echo "</div>";
-											    	
-											}
-										?>
+  											<div class="card-body">
+  												<!-- call user login form -->
+  												<?php if ( ! is_user_logged_in() ) { 
+  													$args = array(
+  														'redirect' => admin_url(), 
+  														'form_id' => 'loginform-custom', 
+  														'label_username' => __( 'Username:' ),
+  														'label_password' => __( 'Password:' ),
+  														'label_remember' => __( 'Remember Me' ),
+  														'label_log_in' => __( 'Log In' ),
+  														'remember' => true
+  													);
+  													wp_login_form( $args ); ?>
+  													<!-- end login form -->
+  													<?php } else { echo "<div class='alert alert-success text-center'>You're already logged in</div>"; ?>
+  											</div>
+  											<?php
+  												echo "<div class='card-footer'>";
+  												echo "<div class='row'>";
+  												echo "<div class='col-sm-6'><span class='k911-loginout'>";
+  													wp_loginout( home_url() );
+  												echo "</span></div>";
+  												echo "<div class='col-sm-6'><span class='k911-adminregister'>";
+													wp_register('', '');
+												echo "</span></div>";
+												echo "</div>";
+												echo "</div>";  	
+											}?>
+									</div>
 								</div>
-		
 							</div>
-						</div>
 				</div>
 				<div id="k911Tags" class="col-lg-12 k911-tags">
-					
-
 					<!-- post and page tags -->
 					<?php the_tags('Related Topics: ' , '&nbsp;', ''); ?>
-
-
+						
 				</div>
-				
-
 				<!-- end WordPress Loop -->
 				<?php endwhile; else : ?>
 					<p><?php esc_html_e( 'We apologize... something went wrong. Please try again.' ); ?></p>
