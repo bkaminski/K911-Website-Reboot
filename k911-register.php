fede6a6770c82bf027a1ecd3f3520986e05c48db
Ben Kaminski <ben@benkaminski.com>
2018-03-04 18:13:58 -0500

    registration page complete for now

--- k911-register.php
+++ k911-register.php
@@ -19,14 +19,14 @@ get_header(); ?>
 								<?php the_title(); ?>
 							</h1>
 						</div>
-							<div class="mt-4 k911-main">
+							<div class="mt-5 k911-main">
 								<div class="col-md-6 mx-auto d-block">
 									<div class="card bg-secondary">
 										<div class="card-header text-center">
 											<h3 class="text-white"><i class="fas fa-paw fa-fw fa-lg mr-1"></i> Create an Account:</h3>
 										</div>
+										<?php the_content(); ?>
 										<div class="card-body">
-											<?php the_content(); ?>
 											<form action="<?php echo site_url('wp-login.php?action=register', 'login_post') ?>" class="user_new" id="user_new" method="post">
 												<div class="form-group">
 													<label>Select a Username</label>
@@ -36,27 +36,28 @@ get_header(); ?>
 													<label>Email Address:</label>
 													<input id="user_email" name="user_email" class="form-control form-control-lg" type="text" placeholder="">
 												</div>
-												<br />
 												<div class="alert alert-warning" role="alert">
 													<em>After clicking the register button below, you will be sent an email to validate your email address. You should receive an email within 5 minutes of successful registration, should you not -- please check you junk email folder first -- then contact us if you're still experiencing problems.</em>
 												</div>
-												<br />
-												<?php do_action('register_form'); ?>
-												<input id="register" type="submit" class="btn btn-info btn-rounded-dark pr-5 pl-5" value="Register">
-											</form>
 										</div>
+										<div class="card-footer">
+											<?php do_action('register_form'); ?>
+											<input id="register" type="submit" class="btn btn-info btn-rounded pr-5 pl-5" value="Register">
+										</div>
+									</form>
 									</div>
 								</div>
-						</div>
+							</div>
+								</div>
+							<div id="k911Tags" class="col-lg-12 k911-tags">
+								<!-- post and page tags -->
+								<?php the_tags('Related Topics: ' , '&nbsp;', ''); ?>	
+							</div>
+							<!-- end WordPress Loop -->
+							<?php endwhile; else : ?>
+								<p><?php esc_html_e( 'We apologize... something went wrong. Please try again.' ); ?></p>
+							<?php endif; ?>
 				</div>
-				<div id="k911Tags" class="col-lg-12 k911-tags">
-					<!-- post and page tags -->
-					<?php the_tags('Related Topics: ' , '&nbsp;', ''); ?>	
-				</div>
-				<!-- end WordPress Loop -->
-				<?php endwhile; else : ?>
-					<p><?php esc_html_e( 'We apologize... something went wrong. Please try again.' ); ?></p>
-				<?php endif; ?>
 			</div>
 		</div>
 	</main>
