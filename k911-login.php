62f0a86dbfa3be74463a90a056e8bba444dbc29c
Ben Kaminski <ben@benkaminski.com>
2018-03-04 16:30:01 -0500

    final fixes to conditional statements on login page

--- k911-login.php
+++ k911-login.php
@@ -7,12 +7,12 @@
  * 
  */
 get_header(); ?>
-	<main class="page-body-content">
-		<div class="container page-main-content">
-			<div class="row">
-				<div class="col-lg-12">
-					<!-- start WordPress Loop -->
-					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<main class="page-body-content">
+	<div class="container page-main-content">
+		<div class="row">
+			<div class="col-lg-12">
+				<!-- start WordPress Loop -->
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 					<div class="alert alert-info mt-3">
 						<h1 class="display-4 text-shadow">
 							<!-- title entered in WordPress page editor -->
@@ -26,20 +26,20 @@ get_header(); ?>
 									<h3><i class="fas fa-paw fa-fw fa-lg mr-1"></i> K911 User Login:</h3>
 								</div>
 									<div class="card-body">
-									<!-- call user login form -->
-									<?php
-										if ( ! is_user_logged_in() ) { 
-											$args = array(
-												'redirect' => admin_url(), 
-												'form_id' => 'loginform-k911',
-												'label_username' => __( 'Username: ' ),
-												'label_password' => __( 'Password: ' ),
-												'label_remember' => __( 'Remember Me? ' ),
-												'label_log_in' => __( 'Log In' ),
-												'remember' => true
-											);
+										<!-- call user login form -->
+										<?php
+											if ( ! is_user_logged_in() ) { 
+												$args = array(
+													'redirect' => admin_url(), 
+													'form_id' => 'loginform-k911',
+													'label_username' => __( 'Username: ' ),
+													'label_password' => __( 'Password: ' ),
+													'label_remember' => __( 'Remember Me? ' ),
+													'label_log_in' => __( 'Log In' ),
+													'remember' => true
+												);
 											wp_login_form( $args ); 
-									?>
+										?>
 									</div>
 									<div class="card-footer">
 										<div class="row">
@@ -49,43 +49,34 @@ get_header(); ?>
 											<div class="col-md">
 												<a class="btn btn-success btn-rounded btn-block mb-2 mt-2" href="<?php echo wp_registration_url(); ?>">Create Account</a>
 											</div>
+											<?php
+											} else {
+												echo "<div class='alert alert-success text-center mt-3 mb-3'>Welcome back $current_user->display_name.<br />";
+												echo "You are currently logged in.<br /><br /><i class='fas fa-check fa-fw fa-3x'></i><br /><br />Please choose an option below.</div>";
+											?>
 										</div>
-										<?php
-										} else {
-											echo "<div class='alert alert-success text-center mt-3 mb-3'>Welcome back $current_user->display_name.<br />";
-											echo "<small>You last logged in on: <br />"; ?>
-											<em><?php get_user_last_login($user_id); ?></em>
-											</small>
-											<br />
-											<br />
-											<?php 
-											echo "You are currently logged in.<br /><br /><i class='fas fa-check fa-fw fa-3x'></i><br /><br />Please choose an option below.</div>";
-										?>
-									</div>
 									<div class="card-footer">
 										<div class="row">
 											<div class="col-md btn-logout">
-												<?php
-													wp_loginout( home_url() ); 
-													echo "</div><div class='col-md btn-admin'>";
-													wp_register('', ''); 
-												}?>
+												<?php wp_loginout( home_url()); ?>
 											</div>
+											<div class="col-md btn-admin">
+												<?php wp_register('', ''); ?>
+											</div>
+									    <?php } ?>
 										</div>
+									</div>
   									</div>
-  								</div>
-						</div>
+  							</div>
+  						</div>
 					</div>
+				<div id="k911Tags" class="col-lg-12 k911-tags">
+					<!-- post and page tags -->
+					<?php the_tags('Related Topics: ' , '&nbsp;', ''); ?>
 				</div>
-			</div>
-		</div>
-		<div id="k911Tags" class="col-lg-12 k911-tags">
-			<!-- post and page tags -->
-			<?php the_tags('Related Topics: ' , '&nbsp;', ''); ?>
-		</div>
-		<!-- end WordPress Loop -->
-		<?php endwhile; else : ?>
-			<p><?php esc_html_e( 'We apologize... something went wrong. Please try again.' ); ?></p>
-		<?php endif; ?>
-	</main>
+				<!-- end WordPress Loop -->
+				<?php endwhile; else : ?>
+					<p><?php esc_html_e( 'We apologize... something went wrong. Please try again.' ); ?></p>
+				<?php endif; ?>
+</main>
 <?php get_footer(); ?>
