aeb68f1772d83ee22eae4188e138266a41a9478f
Ben Kaminski <ben@benkaminski.com>
2018-03-07 21:57:12 -0500

    fixed validation errors on lost password page

--- k911-lostpassword.php
+++ k911-lostpassword.php
@@ -7,64 +7,6 @@
  * 
  */
 get_header(); ?>
-<?php
-        global $wpdb;
-        
-        $error = '';
-        $success = '';
-        
-        // check if we're in reset form
-        if( isset( $_POST['action'] ) && 'reset' == $_POST['action'] ) 
-        {
-            $email = trim($_POST['user_login']);
-            
-            if( empty( $email ) ) {
-                $error = 'Enter a username or e-mail address..';
-            } else if( ! is_email( $email )) {
-                $error = 'Invalid username or e-mail address.';
-            } else if( ! email_exists( $email ) ) {
-                $error = 'There is no user registered with that email address.';
-            } else {
-                
-                $random_password = wp_generate_password( 12, false );
-                $user = get_user_by( 'email', $email );
-                
-                $update_user = wp_update_user( array (
-                        'ID' => $user->ID, 
-                        'user_pass' => $random_password
-                    )
-                );
-                
-                // if  update user return true then lets send user an email containing the new password
-                if( $update_user ) {
-                    $to = $email;
-                    $subject = 'Your new password';
-                    $sender = get_option('name');
-                    
-                    $message = 'Your new password is: '.$random_password;
-                    
-                    $headers[] = 'MIME-Version: 1.0' . "\r\n";
-                    $headers[] = 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-                    $headers[] = "X-Mailer: PHP \r\n";
-                    $headers[] = 'From: '.$sender.' < '.$email.'>' . "\r\n";
-                    
-                    $mail = wp_mail( $to, $subject, $message, $headers );
-                    if( $mail )
-                        $success = 'Check your email address for you new password.';
-                        
-                } else {
-                    $error = 'Oops something went wrong updaing your account.';
-                }
-                
-            }
-            
-            if( ! empty( $error ) )
-                echo '<div class="message"><p class="error"><strong>ERROR:</strong> '. $error .'</p></div>';
-            
-            if( ! empty( $success ) )
-                echo '<div class="error_login"><p class="success">'. $success .'</p></div>';
-        }
-    ?>
 	<main class="page-body-content">
 		<div class="container page-main-content">
 			<div class="row">
@@ -86,6 +28,64 @@ get_header(); ?>
 											<h3 class="text-white"><i class="fas fa-paw fa-fw fa-lg mr-1"></i> Lost Password:</h3>
 										</div>
 										<div class="card-body">
+											<?php
+										        global $wpdb;
+										        
+										        $error = '';
+										        $success = '';
+										        
+										        // check if we're in reset form
+										        if( isset( $_POST['action'] ) && 'reset' == $_POST['action'] ) 
+										        {
+										            $email = trim($_POST['user_login']);
+										            
+										            if( empty( $email ) ) {
+										                $error = 'Please enter a username or e-mail address..';
+										            } else if( ! is_email( $email )) {
+										                $error = 'That username was not found in our database. Please ensure it was entered correctly and try again. Usernames are case-sensitive. If you are still experincing problems, please contact us. ';
+										            } else if( ! email_exists( $email ) ) {
+										                $error = 'There is no user registered with that email address. Please create an account or contact us for assistance.';
+										            } else {
+										                
+										                $random_password = wp_generate_password( 12, false );
+										                $user = get_user_by( 'email', $email );
+										                
+										                $update_user = wp_update_user( array (
+										                        'ID' => $user->ID, 
+										                        'user_pass' => $random_password
+										                    )
+										                );
+										                
+										                // if  update user return true then lets send user an email containing the new password
+										                if( $update_user ) {
+										                    $to = $email;
+										                    $subject = 'Your new password';
+										                    $sender = get_option('name');
+										                    
+										                    $message = 'Your new password is: '.$random_password;
+										                    
+										                    $headers[] = 'MIME-Version: 1.0' . "\r\n";
+										                    $headers[] = 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+										                    $headers[] = "X-Mailer: PHP \r\n";
+										                    $headers[] = 'From: '.$sender.' < '.$email.'>' . "\r\n";
+										                    
+										                    $mail = wp_mail( $to, $subject, $message, $headers );
+										                    if( $mail )
+										                        $success = 'Check your email address for you new password.';
+										                        
+										                } else {
+										                    $error = 'Oops something went wrong updaing your account.';
+										                }
+										                
+										            }
+										            
+										            if( ! empty( $error ) )
+										                echo '<div class="alert alert-danger"><strong>ERROR:</strong> '. $error .'</div>';
+										            
+										            if( ! empty( $success ) )
+										                echo '<div class="alert alert-success">'. $success .'</div>';
+										        }
+										    ?>
 											<form method="post">
 												<fieldset class="form-group">
 													<p>Please enter your username or email address. You will receive a link to create a new password via email.</p>
